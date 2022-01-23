6a8cf8e3735ee62cf429b202c72c1808f4cb8304
Abhinav Singh <abhinavsingh6633@gmail.com>
2022-01-24 00:59:53 +0530

    first name

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/navbar";
+import Product from "./components/Product";
+import ProductList from "./components/ProductList";
+//import Footer from "./components/Footer";
+import React, { useState } from "react";
 
 function App() {
+  const products = [
+    {
+      price: 79999,
+      name: "Iphone 13",
+      quantity: 0,
+    },
+    {
+      price: 9999,
+      name: "Redmi Note 8",
+      quantity: 0,
+    },
+  ];
+  const [productList, setProductList] = useState(products);
+ async function incrementQuantity(index){
+    let newProductList =  [...products];
+    
+    if(!!newProductList[index]){
+     
+   newProductList[index].quantity=4;
+    console.log(newProductList,"index")
+     setProductList(newProductList);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <main className="container mt-5">
+        <ProductList
+          products={productList}
+          incrementQuantity={(e)=>{incrementQuantity(e)}}
+        />
+      </main>
+      {/*<Footer/>*/}
+    </>
   );
 }
 
